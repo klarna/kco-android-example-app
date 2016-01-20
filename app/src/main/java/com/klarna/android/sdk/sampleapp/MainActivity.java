2154b5f9eaad5331690eb209909a1b4bb66d1ed6
Peter Törnhult <peter.tornhult@klarna.com>
2016-01-20 14:50:40 +0100

    Adding a WebView and initialization of KlarnaCheckout, removing FAB and menu

--- app/src/main/java/com/klarna/android/sdk/sampleapp/MainActivity.java
+++ app/src/main/java/com/klarna/android/sdk/sampleapp/MainActivity.java
@@ -1,52 +1,58 @@
 package com.klarna.android.sdk.sampleapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import com.klarna.checkout.sdk.KlarnaCheckout;
+import com.klarna.checkout.sdk.SignalListener;
+
+import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main";
+
+    private TextView mStatusText;
+    private WebView mWebView;
+
+    private KlarnaCheckout mKlarnaCheckout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mStatusText = (TextView)findViewById(R.id.status_text);
+        mWebView = (WebView)findViewById(R.id.web_view);
+
+        mKlarnaCheckout = new KlarnaCheckout(this, mWebView);
+//        mKlarnaCheckout.attachToActivity(this);
+//        mKlarnaCheckout.setGooglePushMessageToken("test");
+//        mKlarnaCheckout.loadCheckoutSnippet("a snippet?");
+        mKlarnaCheckout.setSignalListener(new SignalListener() {
+            private static final String TAG = "SignalListener";
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onSignal(String s, JSONArray jsonArray) {
+                Log.d(TAG, "Got a signal: " + s + ", with params: " + (jsonArray!=null ? jsonArray.toString() : null));
+                mStatusText.setText(s);
             }
         });
+
+        mWebView.loadUrl("https://www.klarnacheckout.com/");
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onDestroy() {
+        if (mKlarnaCheckout!=null) {
+            mKlarnaCheckout.Destroy();
         }
-
-        return super.onOptionsItemSelected(item);
+        super.onDestroy();
     }
 }
