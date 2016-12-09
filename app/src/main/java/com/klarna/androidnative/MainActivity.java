41b1b1d25a82e223e96a861e53bf61905b1451c0
Neal Sanche <neal.sanche@robotsandpencils.com>
2016-12-09 16:11:39 -0700

    Changed flow to use another activity.

--- app/src/main/java/com/klarna/androidnative/MainActivity.java
+++ app/src/main/java/com/klarna/androidnative/MainActivity.java
@@ -1,30 +1,17 @@
 package com.klarna.androidnative;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-import com.klarna.checkout.KlarnaCheckout;
-import com.klarna.checkout.SignalListener;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity implements HandshakeListener {
 
@@ -32,7 +19,8 @@ public class MainActivity extends AppCompatActivity implements HandshakeListener
     private static final String KCO_NATIVE = "KCO_NATIVE";
 
     private FloatingActionButton mFab;
-    private String mSnippet;
+    private WebView mWebView;
+    private boolean mConfirmationSnippetLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,40 +30,10 @@ public class MainActivity extends AppCompatActivity implements HandshakeListener
         setSupportActionBar(toolbar);
 
         mFab = (FloatingActionButton) findViewById(R.id.fab);
-        mFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                mFab.hide();
-
-                findViewById(R.id.scroller).setVisibility(View.VISIBLE);
-
-                String snippet = mSnippet;
-                KlarnaCheckout checkout = new KlarnaCheckout(MainActivity.this);
-                checkout.setSignalListener(new SignalListener() {
-                    @Override
-                    public void onSignal(String eventName, JSONObject jsonObject) {
-                        if (eventName.equals("complete")) {
-                            try {
-                                String url = jsonObject.getString("uri");
-                                loadConfirmationSnippet(url);
-                            } catch (JSONException e) {
-                                Log.e(e.getMessage(), e.toString());
-                            }
-                        }
-                    }
-                });
-
-                checkout.setSnippet(snippet);
-
-                ViewGroup placeholder = (ViewGroup) findViewById(R.id.klarna_placeholder);
-                placeholder.addView(checkout.getView());
-            }
-        });
         mFab.hide();
 
-        WebView webView = setupWebView();
-        webView.loadUrl("http://klarnacheckout.com/");
+        mWebView = setupWebView();
+        mWebView.loadUrl("http://klarnacheckout.com/");
     }
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -113,10 +71,6 @@ public class MainActivity extends AppCompatActivity implements HandshakeListener
         };
     }
 
-    private void loadConfirmationSnippet(String url) {
-        Log.e("MainActivity", "loadConfirmationSnippet: " + url);
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -139,38 +93,29 @@ public class MainActivity extends AppCompatActivity implements HandshakeListener
         return super.onOptionsItemSelected(item);
     }
 
-    public String readAsset(String assetFileName) {
-        StringBuilder buf = new StringBuilder();
-        try {
-            InputStream json = getAssets().open(assetFileName);
-            BufferedReader in = new BufferedReader(new InputStreamReader(json, "UTF-8"));
-            String str;
-
-            while ((str = in.readLine()) != null) {
-                buf.append(str);
-            }
-
-            in.close();
-        } catch (IOException ex) {
-            Log.e("MainActivity", "Unable to read " + assetFileName + " asset: " + ex.getMessage(), ex);
-        }
-        return buf.toString();
-    }
-
     @Override
     public void onHandshake(String snippet) {
-        mSnippet = snippet;
-        mFab.show();
+        if (!mConfirmationSnippetLoading) {
+            Intent intent = new Intent(this, CheckoutActivity.class);
+            intent.putExtra("snippet", snippet);
+            startActivityForResult(intent, 100);
+            mConfirmationSnippetLoading = false;
+        }
     }
 
     @Override
     public void onBackPressed() {
-        View view = findViewById(R.id.scroller);
-        if (mSnippet != null && view.getVisibility() == View.VISIBLE) {
-            mFab.show();
-            view.setVisibility(View.GONE);
-            return;
-        }
         super.onBackPressed();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            String url = data.getStringExtra("url");
+            mConfirmationSnippetLoading = true;
+            mWebView.loadUrl(url);
+        }
     }
 }
